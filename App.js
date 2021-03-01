164f147f16f3078959a1e96e71e2b1f068e8f4ff
Berat Koçak <42033267+Reitnos@users.noreply.github.com>
2021-03-01 19:48:06 +0300

    Progress on scenes

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import React, {useState} from 'react';
+import Home from "./screens/home";
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
+
+const getFonts = () => Font.loadAsync({
+  'Nunito-Regular': require("./assets/fonts/Nunito-Regular.ttf"),
+    'Nunito-Bold': require("./assets/fonts/Nunito-Bold.ttf")
+});
+
+
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [fontsLoaded,setFontsLoaded] = useState(false);
+  if(fontsLoaded){
+
+      return (
+        <Home/>
+      );
+  }
+  else{
+    return(
+
+      <AppLoading 
+      
+      startAsync = {getFonts}
+      onFinish = {() => setFontsLoaded(true)}
+      
+
+      />
+    )
+     
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+
---
+++ screens/reviewDetails.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {StyleSheet,View,Text} from 'react-native'
+
+export default function ReviewDetails(){
+    return(
+        <View>
+            <Text style = {globalStyles.container}> ReviewDetails Screen </Text>
+        </View>
+    )
+}
